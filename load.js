0fd458327463b1b11fd33a1fb40836e3a346cb5b
NGS-Adryan <ngs.adryan@gmail.com>
2024-06-21 14:36:30 -0300

    criando arquivo encarregado de carregar as mensagens
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

---
+++ load.js
@@ -0,0 +1,16 @@
+const messagens = [
+    {name: "Adryan", message: "Oi."},
+    {name: "Nicole", message: "Oi."},
+    {name: "Adryan", message: "Td bem?"},
+    {name: "Nicole", message: "Sim e tu?"},
+    {name: "Adryan", message: "bem"},
+    {name: "Nicole", message: 
+`*CCO-Informa*
+- 665/004 665 carro 2545
+- 665 das 16:30 BC
+- Informo que viagem não realizada
+- Porque eu não quis realizar.
+`}
+];
+
+export { messagens }
